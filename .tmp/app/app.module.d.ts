09e26c0d79f9232f94cef0ad1f8d6dbe75f70c6d
Tanya Gray <tanyagray.nz@gmail.com>
2016-10-13 11:15:13 +1300

    Removing temp files and build from version control

--- .tmp/app/app.module.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const firebaseConfig: {
-    apiKey: string;
-    authDomain: string;
-    databaseURL: string;
-    storageBucket: string;
-    messagingSenderId: string;
-};
-export declare class AppModule {
-}
